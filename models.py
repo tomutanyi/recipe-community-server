be5855cb32c181179b87f6fe8b7f01fbf1c48179
tomutanyi <tomutanyi@gmail.com>
2024-09-19 19:37:44 +0300

    added ecipe-listing model

--- models.py
+++ models.py
@@ -39,3 +39,21 @@ class User(db.Model, SerializerMixin):
         return bcrypt.check_password_hash(self._password_hash,password.encode("utf-8"))
     
 
+class RecipeListing(db.Model, SerializerMixin):
+
+    __tablename__ = "recipe_listing"
+
+    serialize_rules = ("-recipe_reviews.recipe_listing", )
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    image_link = db.Column(db.String(255))
+    ingredients = db.Column(db.String(100))
+    instructions = db.Column(db.String(100))
+    dietary_type = db.Column(db.String(), nullable=False)
+
+    recipe_reviews = db.relationship('RecipeReview', backref='reccipe_listing')
+
+    def _repr_(self):
+        return f'Recipe: {self.name}, Ingredients: {self.ingredients}, Instructions: {self.instructions}, Diet: {self.dietary_type}'
+    
