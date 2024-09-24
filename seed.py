acace373fbd861b1157917a22c447fb457ff1dcf
tomutanyi <tomutanyi@gmail.com>
2024-09-24 08:09:01 +0300

    changed to smaller image sizes

--- seed.py
+++ seed.py
@@ -30,77 +30,77 @@ def seed_recipes():
             "name": "Spaghetti Aglio e Olio",
             "ingredients": "400g spaghetti, 4 garlic cloves, sliced, 1/2 cup olive oil, 1 tsp red pepper flakes, Salt, to taste, Fresh parsley, chopped, Grated Parmesan (optional)",
             "instructions": "Cook spaghetti in salted water until al dente. Reserve 1 cup of pasta water and drain the rest. In a pan, heat olive oil and sauté garlic until golden. Add red pepper flakes and cooked spaghetti to the pan. Toss to combine. Add reserved pasta water as needed for moisture. Season with salt and garnish with parsley and Parmesan.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109579/recipe-community/pexels-bulbfish-1143754_ivuisl.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154212/recipe-community/11-recipes-small-size/9094_tfrvka.jpg",
             "dietary_type": "Vegetarian"
         },
         {
             "name": "Chicken Tacos",
             "ingredients": "500g chicken breast, diced, 1 tbsp olive oil, 1 tsp cumin, 1 tsp paprika, Salt and pepper, to taste, Corn tortillas, Toppings: avocado, cilantro, lime, salsa",
             "instructions": "Heat olive oil in a pan. Add chicken and season with cumin, paprika, salt, and pepper. Cook until chicken is browned and cooked through. Warm tortillas in another pan. Assemble tacos with chicken and desired toppings.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109534/recipe-community/pexels-fox-58267-1265626_dvmsjb.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154211/recipe-community/11-recipes-small-size/5057_uelnjw.jpg",
             "dietary_type": "Non-Vegetarian"
         },
         {
             "name": "Vegetable Stir-Fry",
             "ingredients": "2 cups mixed vegetables (bell peppers, broccoli, carrots), 2 tbsp soy sauce, 1 tbsp sesame oil, 2 garlic cloves, minced, 1 tsp ginger, minced, Cooked rice, for serving",
             "instructions": "Heat sesame oil in a large pan. Add garlic and ginger, sautéing until fragrant. Add mixed vegetables and stir-fry for 5-7 minutes. Pour in soy sauce and toss to coat. Serve over cooked rice.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109533/recipe-community/pexels-sebastian-coman-photography-1598188-3791089_e3e7k8.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154211/recipe-community/11-recipes-small-size/2148739212_tvyup3.jpg",
             "dietary_type": "Vegan"
         },
         {
             "name": "Caprese Salad",
             "ingredients": "4 ripe tomatoes, sliced, 250g mozzarella, sliced, Fresh basil leaves, 3 tbsp olive oil, Balsamic vinegar (optional), Salt and pepper, to taste",
             "instructions": "Arrange tomato and mozzarella slices on a plate, alternating them. Tuck basil leaves between the slices. Drizzle with olive oil and balsamic vinegar. Season with salt and pepper.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109530/recipe-community/pexels-enginakyurt-2347311_htox58.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154210/recipe-community/11-recipes-small-size/19739_andqr8.jpg",
             "dietary_type": "Vegetarian"
         },
         {
             "name": "Beef Stroganoff",
             "ingredients": "500g beef sirloin, sliced, 1 onion, chopped, 200g mushrooms, sliced, 2 cups beef broth, 1 cup sour cream, 2 tbsp flour, Salt and pepper, to taste, Egg noodles, for serving",
             "instructions": "Sauté onion and mushrooms in a pan until soft. Add beef and cook until browned. Sprinkle flour over beef and stir. Gradually add beef broth, cooking until thickened. Remove from heat and stir in sour cream. Season with salt and pepper. Serve over egg noodles.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109530/recipe-community/pexels-valeriya-1833336_amtcm5.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154210/recipe-community/11-recipes-small-size/alex_g7by0u.jpg",
             "dietary_type": "Non-Vegetarian"
         },
         {
             "name": "Quinoa Salad",
             "ingredients": "1 cup quinoa, 2 cups water, 1 cup cherry tomatoes, halved, 1 cucumber, diced, 1/4 cup red onion, chopped, 1/4 cup feta cheese, crumbled, 3 tbsp olive oil, Juice of 1 lemon, Salt and pepper, to taste",
             "instructions": "Rinse quinoa and cook with water until fluffy. Let it cool. In a bowl, combine quinoa, tomatoes, cucumber, onion, and feta. Drizzle with olive oil and lemon juice. Season with salt and pepper.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109513/recipe-community/pexels-valeriya-1860205_nl2axk.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154210/recipe-community/11-recipes-small-size/2148784869_b7pyoq.jpg",
             "dietary_type": "Vegetarian"
         },
         {
             "name": "Mushroom Risotto",
             "ingredients": "1 cup Arborio rice, 1 onion, chopped, 200g mushrooms, sliced, 4 cups vegetable broth, 1/2 cup white wine, 1/2 cup Parmesan cheese, grated, 2 tbsp butter, Salt and pepper, to taste",
             "instructions": "Sauté onion and mushrooms in butter until soft. Add Arborio rice and stir for 1-2 minutes. Pour in wine and let it absorb. Gradually add broth, stirring constantly until rice is creamy. Stir in Parmesan and season with salt and pepper.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109488/recipe-community/pexels-harry-dona-2338407_wenkeb.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154208/recipe-community/11-recipes-small-size/7397_dmeiu3.jpg",
             "dietary_type": "Vegetarian"
         },
         {
             "name": "Lentil Soup",
             "ingredients": "1 cup lentils, rinsed, 1 onion, chopped, 2 carrots, diced, 2 celery stalks, diced, 4 cups vegetable broth, 2 garlic cloves, minced, 1 tsp thyme, Salt and pepper, to taste",
             "instructions": "In a pot, sauté onion, carrots, and celery until soft. Add garlic and thyme, cooking for another minute. Stir in lentils and broth. Bring to a boil, then simmer until lentils are tender. Season with salt and pepper.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109488/recipe-community/pexels-fotios-photos-1279330_zpht6q.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154203/recipe-community/11-recipes-small-size/17403_a5to6b.jpg",
             "dietary_type": "Vegan"
         },
         {
             "name": "Pancakes",
             "ingredients": "1 cup flour, 2 tbsp sugar, 1 tbsp baking powder, 1/2 tsp salt, 1 cup milk, 1 egg, 2 tbsp melted butter",
             "instructions": "In a bowl, mix flour, sugar, baking powder, and salt. In another bowl, whisk milk, egg, and melted butter. Combine with dry ingredients. Heat a pan and pour batter to form pancakes. Cook until bubbles form, then flip. Serve with syrup and berries.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109487/recipe-community/pexels-dana-tentis-118658-262959_nwax8v.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154202/recipe-community/11-recipes-small-size/18809_yw6dtt.jpg",
             "dietary_type": "Vegetarian"
         },
         {
             "name": "Stuffed Bell Peppers",
             "ingredients": "4 bell peppers, halved and seeded, 1 cup cooked rice, 500g ground beef or turkey, 1 can diced tomatoes, 1 tsp Italian seasoning, 1 cup shredded cheese",
             "instructions": "Preheat oven to 375°F (190°C). In a pan, cook ground meat until browned. Add tomatoes, rice, and Italian seasoning. Fill bell pepper halves with the mixture and place in a baking dish. Top with cheese and bake for 30 minutes.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109477/recipe-community/pexels-catscoming-674574_aj80bn.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154201/recipe-community/11-recipes-small-size/18813_xftpy5.jpg",
             "dietary_type": "Non-Vegetarian"
         },
         {
             "name": "Egg Fried Rice",
             "ingredients": "2 cups cooked rice, 2 eggs, beaten, 1 cup mixed vegetables, 2 tbsp soy sauce, 2 green onions, sliced, 2 tbsp oil",
             "instructions": "Heat oil in a pan, scramble the eggs, and set aside. In the same pan, add vegetables and stir-fry for 3-5 minutes. Add rice and soy sauce, stirring well. Mix in scrambled eggs and green onions.",
-            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727109459/recipe-community/pexels-dana-tentis-118658-1213710_sajx1q.jpg",
+            "image_link": "https://res.cloudinary.com/dzpurt5ec/image/upload/v1727154202/recipe-community/11-recipes-small-size/34705_ouqsf9.jpg",
             "dietary_type": "Vegetarian"
         }
     ]
